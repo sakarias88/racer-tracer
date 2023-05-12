73d918ae0bec69cd41a973bb109ad9fbcdc842c7
Sakarias Johansson <sakarias.johansson@goodbyekansas.com>
2023-05-12 18:21:40 +0200

    🐛 Fix AABB not getting updated
    Since you can move the objects it would make sense if the bounding
    boxes moved with them. This also fixes the movable spheres not moving
    when trying to move them. They just had their own pos that did not
    change.

--- racer-tracer/src/geometry/sphere.rs
+++ racer-tracer/src/geometry/sphere.rs
@@ -5,30 +5,24 @@ use crate::aabb::Aabb;
 use crate::geometry::HitRecord;
 use crate::ray::Ray;
 use crate::scene::HittableSceneObject;
+use crate::scene::PartialSceneObject;
 use crate::scene::SceneObject;
 use crate::vec3::Vec3;
 
 #[derive(Clone)]
 pub struct Sphere {
     radius: f64,
-    aabb: Aabb,
 }
 
 impl Sphere {
-    pub fn new(pos: &Vec3, radius: f64) -> Self {
-        Self {
-            radius,
-            aabb: Aabb::new(
-                pos - Vec3::new(radius, radius, radius),
-                pos + Vec3::new(radius, radius, radius),
-            ),
-        }
+    pub fn new(radius: f64) -> Self {
+        Self { radius }
     }
 }
 
 impl HittableSceneObject for Sphere {
     fn obj_hit(&self, obj: &SceneObject, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
-        let oc = ray.origin() - obj.pos;
+        let oc = ray.origin() - obj.pos();
         let a = ray.direction().length_squared();
         let half_b = oc.dot(ray.direction());
         let c = oc.length_squared() - self.radius * self.radius;
@@ -51,14 +45,19 @@ impl HittableSceneObject for Sphere {
         }
 
         let point = ray.at(root);
-        let outward_normal = (point - obj.pos) / self.radius;
+        let outward_normal = (point - obj.pos()) / self.radius;
 
         let mut hit_record = HitRecord::new(point, root, Arc::clone(&obj.material));
         hit_record.set_face_normal(ray, outward_normal);
         Some(hit_record)
     }
 
-    fn bounding_box(&self, _time0: f64, _time1: f64) -> &Aabb {
-        &self.aabb
+    fn update_pos(&mut self, _pos_delta: &Vec3) {}
+
+    fn create_bounding_box(&self, obj: &PartialSceneObject, _time0: f64, _time1: f64) -> Aabb {
+        Aabb::new(
+            obj.pos - Vec3::new(self.radius, self.radius, self.radius),
+            obj.pos + Vec3::new(self.radius, self.radius, self.radius),
+        )
     }
 }
